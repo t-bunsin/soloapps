9f2e1c851749792be1b8569805fda5b0ce4a3af6
tbunsin <apps.bunsin@gmail.com>
2025-02-18 22:20:20 +0700

    devOps Process include DockerFile and docker-compose.yml

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
